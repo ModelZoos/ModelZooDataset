c30b83d3a2d2cbd5c38d693a30e6872db1075d5b
Konstantin Schürholt <55194201+kschuerholt@users.noreply.github.com>
2022-08-25 15:53:56 +0200

    Add support for large datasets

--- code/model_definitions/def_NN_experiment.py
+++ code/model_definitions/def_NN_experiment.py
@@ -1,4 +1,3 @@
-import json
 from ray.tune import Trainable
 
 import torch
@@ -7,17 +6,16 @@ import sys
 # print(f"sys path in experiment: {sys.path}")
 from pathlib import Path
 
-from def_FastTensorDataLoader import FastTensorDataLoader
+from .def_FastTensorDataLoader import FastTensorDataLoader
 
-from def_net import NNmodule
-
-sys.path.append('./../')
+from .def_net import NNmodule
 
 from checkpoints_to_datasets.dataset_auxiliaries import (
     vectorize_checkpoint,
     vector_to_checkpoint,
 )
 
+import json
 
 """
 define Tune Trainable
@@ -31,11 +29,6 @@ class NN_tune_trainable(Trainable):
         self.seed = config["seed"]
         self.cuda = config["cuda"]
 
-        # init model
-        self.NN = NNmodule(
-            config=self.config, cuda=self.cuda, seed=self.seed, verbosity=0
-        )
-
         if config.get("dataset::dump", None) is not None:
             # print(f"loading dataset from {config['dataset::dump']}")
             # load dataset from file
@@ -43,6 +36,7 @@ class NN_tune_trainable(Trainable):
             dataset = torch.load(config["dataset::dump"])
             self.trainset = dataset["trainset"]
             self.testset = dataset["testset"]
+            self.valset = dataset.get("valset", None)
         else:
 
             data_path = config["training::data_path"]
@@ -73,23 +67,43 @@ class NN_tune_trainable(Trainable):
                     "### WARNING ### : using tensor dataloader without cuda. probably slow"
                 )
             # create new tensor datasets
-            self.trainset = torch.utils.data.TensorDataset(
-                train_data, train_labels)
-            self.testset = torch.utils.data.TensorDataset(
-                test_data, test_labels)
+            self.trainset = torch.utils.data.TensorDataset(train_data, train_labels)
+            self.testset = torch.utils.data.TensorDataset(test_data, test_labels)
 
         # instanciate Tensordatasets
-        self.trainloader = FastTensorDataLoader(
-            dataset=self.trainset,
-            batch_size=config["training::batchsize"],
-            shuffle=True,
-        )
-        self.testloader = FastTensorDataLoader(
-            dataset=self.testset, batch_size=len(self.testset), shuffle=False
-        )
+        self.dl_type = config.get("training::dataloader", "tensor")
+        if self.dl_type == "tensor":
+            self.trainloader = FastTensorDataLoader(
+                dataset=self.trainset,
+                batch_size=config["training::batchsize"],
+                shuffle=True,
+                num_workers=self.config.get("testloader::workers", 2),
+            )
+            self.testloader = FastTensorDataLoader(
+                dataset=self.testset, batch_size=len(self.testset), shuffle=False
+            )
+        else:
+            self.trainloader = torch.utils.data.DataLoader(
+                dataset=self.trainset,
+                batch_size=self.config["training::batchsize"],
+                shuffle=True,
+                num_workers=self.config.get("testloader::workers", 2),
+            )
+            self.testloader = torch.utils.data.DataLoader(
+                dataset=self.testset, batch_size=self.config["training::batchsize"], shuffle=False
+            )
+            if self.valset is not None:
+                self.valloader = torch.utils.data.DataLoader(
+                    dataset=self.valset, batch_size=self.config["training::batchsize"], shuffle=False
+                )
+        # TODO: allow different dataloaders for CIFAR
 
-        # drop inital checkpoint
-        self.save()
+        config["scheduler::steps_per_epoch"] = len(self.trainloader)
+
+        # init model
+        self.NN = NNmodule(
+            config=self.config, cuda=self.cuda, seed=self.seed, verbosity=0
+        )
 
         # run first test epoch and log results
         self._iteration = -1
@@ -99,13 +113,12 @@ class NN_tune_trainable(Trainable):
         # run one training epoch
         if self._iteration < 0:
             print("test first validation mode")
-            loss_train, acc_train = -999, -999
+            loss_train, acc_train = self.NN.test_epoch(self.trainloader, 0)
 
         else:
-            loss_train, acc_train = self.NN.train(
-                self.trainloader, 0, idx_out=10)
+            loss_train, acc_train = self.NN.train_epoch(self.trainloader, 0, idx_out=10)
         # run one test epoch
-        loss_test, acc_test = self.NN.test(self.testloader, 0)
+        loss_test, acc_test = self.NN.test_epoch(self.testloader, 0)
 
         result_dict = {
             "train_loss": loss_train,
@@ -113,35 +126,38 @@ class NN_tune_trainable(Trainable):
             "test_loss": loss_test,
             "test_acc": acc_test,
         }
+        if self.valset is not None:
+            loss_val, acc_val = self.NN.test_epoch(self.valloader, 0)
+            result_dict["val_loss"] = loss_val
+            result_dict["val_acc"] = acc_val
 
         return result_dict
 
-    def save_checkpoint(self, experiment_dir):
+    def save_checkpoint(self, tmp_checkpoint_dir):
         # define checkpoint path
-        path = Path(experiment_dir).joinpath("checkpoints")
+        path = Path(tmp_checkpoint_dir).joinpath("checkpoints")
         # save model state dict
         torch.save(self.NN.model.state_dict(), path)
         # save optimizer
-        path = Path(experiment_dir).joinpath("optimizer")
+        path = Path(tmp_checkpoint_dir).joinpath("optimizer")
         torch.save(self.NN.optimizer.state_dict(), path)
 
         # tune apparently expects to return the directory
-        return experiment_dir
+        return tmp_checkpoint_dir
 
-    def load_checkpoint(self, experiment_dir):
+    def load_checkpoint(self, tmp_checkpoint_dir):
         # define checkpoint path
-        path = Path(experiment_dir).joinpath("checkpoints")
+        path = Path(tmp_checkpoint_dir).joinpath("checkpoints")
         # save model state dict
         checkpoint = torch.load(path)
         self.NN.model.load_state_dict(checkpoint)
         # load optimizer
         try:
-            path = Path(experiment_dir).joinpath("optimizer")
+            path = Path(tmp_checkpoint_dir).joinpath("optimizer")
             opt_dict = torch.load(path)
             self.NN.optimizer.load_state_dict(opt_dict)
         except:
-            print(
-                f"Could not load optimizer state_dict. (not found at path {path})")
+            print(f"Could not load optimizer state_dict. (not found at path {path})")
 
     def reset_config(self, new_config):
         success = False
